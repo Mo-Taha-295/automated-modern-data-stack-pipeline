9c2635566b0d93479961e73e40b7df363f10b227
Mohammed Taha <motaha295d@gmail.com>
2026-06-27 02:22:52 +0300

    feat: convert listings source model to incremental & Changed materialization from view to incrementa l& Added unique_key constraint to prevent duplicates & Implemented filtering logic using CREATED_AT boundary condition

--- Airbnb_DBT/models/bronze/bronze_listings.sql
+++ Airbnb_DBT/models/bronze/bronze_listings.sql
@@ -1,2 +1,10 @@
+{{config(
+    materialized='incremental' , 
+    unique_key='LISTING_ID'
+)}}
 select * 
 from {{source('airbnb_data', 'LISTINGS')}}
+{% if is_incremental() %}
+  -- We use >= to ensure no data falling on the exact max timestamp boundary is missed
+  where CREATED_AT >= (select max(CREATED_AT) from {{ this }})
+{% endif %}
